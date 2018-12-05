c1eb8da3dd71be98be8d244f7083113a9832bc9c
Travis Lang <tlang50@icloud.com>
2018-12-05 16:37:40 -0600

    add more material-ui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import InputForm from '../InputForm/InputForm';
 import SnackList from '../SnackList/SnackList';
-
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>Snack Table</h2>
-        <InputForm />
-        <SnackList />
-      </div>
+        <Router>
+            <div className="App">
+                <Link to='/'>Form</Link>
+                <Link to='/snacks'>Snacks</Link>
+                <h2>Snack Table</h2>
+                <Route path='/' exact component={InputForm} />
+                <Route path='/snacks' component={SnackList} />
+            </div>
+        </Router>
     );
   }
 }
--- src/components/SnackList/SnackList.jsx
+++ src/components/SnackList/SnackList.jsx
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        maxWidth: '360px',
+        backgroundColor: theme.palette.background.paper,
+    },
+});
+
 class SnackList extends Component {
     
 
     render() {
+        const { classes } = this.props;
         let snacks = this.props.reduxStore.snacks.map( (item, i) => {
-            return <li key={i}>{item}</li>
+            return <ListItem key={i} divider><ListItemText primary={item} /></ListItem>
         })
         return (
-            <ul>
-                {snacks}
-            </ul>
+            <Grid container 
+                direction="column"
+                justify="center"
+                alignItems="center" className={classes.root}>
+                <Grid item xs={12}>
+                    <List className={classes.root}>
+                        {snacks}
+                    </List>
+                </Grid>
+            </Grid>
+            
         )
     }
 }
@@ -23,4 +48,8 @@ const mapReduxStateToProps = (reduxStore) => {
     }
 }
 
-export default connect(mapReduxStateToProps)(SnackList);
+SnackList.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default connect(mapReduxStateToProps)(withStyles(styles)(SnackList));
